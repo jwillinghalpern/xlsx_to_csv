2e3e9968f58af3bc16326c8ff3d635373ec3cb82
Joshua Willing <jwillinghalpern@gmail.com>
2023-10-17 22:47:39 -0700

    start custom parser

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use calamine::{open_workbook, Reader, Xlsx};
+use calamine::{open_workbook, DataType, Reader, Xlsx};
 use clap::Parser;
 use csv::Writer;
 
@@ -18,6 +18,47 @@ struct Cli {
     sheet: Option<String>,
 }
 
+fn parse_cell(cell: &DataType) -> String {
+    match cell {
+        calamine::DataType::Int(x) => x.to_string(),
+        calamine::DataType::Float(x) => x.to_string(),
+        calamine::DataType::String(x) => x.clone(),
+        calamine::DataType::Bool(x) => match x {
+            true => "1".to_string(),
+            false => "0".to_string(),
+        },
+        calamine::DataType::DateTime(x) => {
+            // TODO: should we determine if the date value is 0 and treat this as just Time instead of DateTime?
+            // TODO: if there is no fractional value, should we treat this as just Date instead of DateTime?
+            // f64 represents days since Jan 1, 1900
+            // convert it to a human readable date
+            let days = *x as i32;
+            let date =
+                chrono::NaiveDate::from_ymd(1900, 1, 1) + chrono::Duration::days(days.into());
+            // get the fractional value of the day
+            let frac = *x - *x as i32 as f64;
+            // convert fractional val to time
+            let time =
+                chrono::NaiveTime::from_num_seconds_from_midnight((frac * 86400.0) as u32, 0);
+            let datetime = chrono::NaiveDateTime::new(date, time);
+            datetime.format("%Y-%m-%d %H:%M:%S").to_string()
+        }
+        calamine::DataType::Duration(x) => {
+            todo!()
+        }
+        calamine::DataType::DateTimeIso(x) => {
+            todo!()
+        }
+        calamine::DataType::DurationIso(x) => {
+            todo!()
+        }
+        calamine::DataType::Error(x) => {
+            todo!()
+        }
+        calamine::DataType::Empty => "".to_string(),
+    }
+}
+
 fn xlsx_to_csv(
     input: &str,
     output: &str,
@@ -33,11 +74,14 @@ fn xlsx_to_csv(
 
     if let Some(Ok(range)) = excel.worksheet_range(&sheet_name) {
         for row in range.rows() {
-            let values: Vec<&str> = row
+            let values = row
                 .iter()
+                .map(parse_cell)
                 // TODO: this currently returns an empty string if get_string fails. Should we error instead?
-                .map(|cell| cell.get_string().unwrap_or_default())
-                .collect();
+                // .map(|cell| cell.get_string().unwrap_or_default())
+                // .map(|cell| cell.as_string().unwrap_or_default())
+                // .map(|cell| cell.to_string())
+                .collect::<Vec<_>>();
             csv_file.write_record(&values)?;
         }
     } else {
